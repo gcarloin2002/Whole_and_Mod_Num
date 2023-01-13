78639ab1b55afa9cbfd788b8e1d8a855344663b3
gcarloin2002 <gcarloin2002@gmail.com>
2023-01-12 23:32:56 -0600

    Update Mod_Num.h

--- Mod_Num.h
+++ Mod_Num.h
@@ -20,7 +20,7 @@ class Mod_Num
 
         void setNum(Whole_Num n); // Sets _number
         void setMod(Whole_Num m); // Sets _modulo
-        
+
         Whole_Num getNum() const; // Gets _number
         Whole_Num getMod() const; // Gets _modulo
 
